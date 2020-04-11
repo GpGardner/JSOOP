66870fa438e4d5a1a7cc6cc36abe726cb1f397b7
George Gardner <gpgardner@yahoo.com>
2020-04-11 12:46:36 -0400

    small example using constructor and prototypes

---
+++ mixins.js
@@ -0,0 +1,40 @@
+const btn_btn = document.getElementById('btn')
+const input_input = document.getElementById('input')
+
+
+function Profile(name, address, city, state, zipcode, avatar) {
+  (this.name = name),
+  (this.address = address),
+  (this.city = city),
+  (this.state = state),
+  (this.zipcode = zipcode),
+  (this.avatar = avatar);
+}
+
+Profile.prototype ={
+  
+  updateProfile : function() {
+    Object.assign(this, this.getProfileUpdate())
+    return this;
+  },
+  
+  getProfileUpdate : function(){
+    let name = prompt('name')
+    let address = prompt('address')
+    let city = prompt('city')
+    let state = prompt('state')
+    let zipcode = prompt('zipcode')
+    
+    return {
+      name, address, city, state, zipcode
+    }
+  },
+}
+
+
+let user = new Profile('George','','Charlotte', 'NC', '28209', 'none' );
+
+// eventually fix button to select user
+// btn.addEventListener('click', function(){
+//   (input_input.value).updateProfile();
+// })
